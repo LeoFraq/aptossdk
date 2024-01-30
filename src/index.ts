6db2cab9ea8688fb9b6d87bb396b7f8c3c97272e
leonardo fraquelli <leonardo.fraquelli1997@gmail.com>
2024-01-30 20:34:39 +0100

    dio p

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-// src/index.ts
+// // src/index.ts
 function greet(name: string): string {
     return `Hello, ${name}!`;
 }
 
 const result = greet("World");
 console.log(result);
+
+
+import { Account, AccountAddress, Aptos, AptosConfig, Network, NetworkToNetworkName } from "@aptos-labs/ts-sdk";
+
+const ALICE_INITIAL_BALANCE = 100_000_000;
+const BOB_INITIAL_BALANCE = 0;
+const TRANSFER_AMOUNT = 1_000_000;
+
+
+const main = async () => {
+    // Setup the client
+    const APTOS_NETWORK: Network = Network.CUSTOM;
+    const config = new AptosConfig({ network: APTOS_NETWORK });
+    const aptos = new Aptos(config);
+    const ledgerInfo = await aptos.getLedgerInfo();
+    console.log("Ledger Info:")
+    console.log(ledgerInfo)
+
+}
+main()
