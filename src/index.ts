c0a93769ee41ca15f0103cef3af6d904ce578e18
leonardo fraquelli <leonardo.fraquelli1997@gmail.com>
2024-01-31 18:42:37 +0100

    faucet

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,15 @@ const BOB_INITIAL_BALANCE = 0;
 const TRANSFER_AMOUNT = 1_000_000;
 
 const fullnodeIP = "http://10.1.2.4:8080/v1/"
+const faucetIP = "http://127.0.0.1:8080/v1/"
 
 const main = async () => {
     // Setup the client
     const APTOS_NETWORK: Network = Network.CUSTOM;
     const config = new AptosConfig({
         network: APTOS_NETWORK,
-        fullnode: fullnodeIP
+        fullnode: fullnodeIP,
+        faucetIP: faucetIP
     });
     const aptos = new Aptos(config);
     const ledgerInfo = await aptos.getLedgerInfo();
@@ -38,13 +40,15 @@ const main = async () => {
     console.log(`Bob's address is: ${bob.accountAddress}`);
 
     // build a transaction
-    const transaction = await aptos.transaction.build.transaction({
-        sender: alice.accountAddress,
-        data: {
-            function: "0x1::coin::transfer",
-            typeArguments: ["0x1::aptos_coin::AptosCoin"],
-            functionArguments: [bob.accountAddress, 100],
-        },
+    const aliceFundTxn = await aptos.faucet.fundAccount({
+        accountAddress: alice.accountAddress,
+        amount: ALICE_INITIAL_BALANCE,
+    });
+    console.log("Alice's fund transaction: ", aliceFundTxn);
+
+    const bobFundTxn = await aptos.faucet.fundAccount({
+        accountAddress: bob.accountAddress,
+        amount: BOB_INITIAL_BALANCE,
     });
 
 }
