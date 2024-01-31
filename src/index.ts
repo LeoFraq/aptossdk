336f7ca408b1ffa2af5dbaa30a1d8d9e165c9d21
leonardo fraquelli <leonardo.fraquelli1997@gmail.com>
2024-01-31 19:24:14 +0100

    faucet

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,15 @@ const BOB_INITIAL_BALANCE = 0;
 const TRANSFER_AMOUNT = 1_000_000;
 
 const fullnodeIP = "http://10.1.2.4:8080/v1"
-// const faucetIP = "http://127.0.0.1:8000/"
+const faucetIP = "http://127.0.0.1:8000/"
 
 const main = async () => {
     // Setup the client
     const APTOS_NETWORK: Network = Network.CUSTOM;
     const config = new AptosConfig({
         network: APTOS_NETWORK,
-        fullnode: fullnodeIP
-        // faucet: faucetIP
+        fullnode: fullnodeIP,
+        faucet: faucetIP
     });
     const aptos = new Aptos(config);
     const ledgerInfo = await aptos.getLedgerInfo();
